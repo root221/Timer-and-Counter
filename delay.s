8dba6080a651959bbb8d8110b29f17baeaaa9370
skye <skye53653@gmail.com>
2016-12-07 11:49:05 +0800

    7-2

--- delay.s
+++ delay.s
@@ -5,17 +5,29 @@
 	.equ RCC_AHB2ENR, 0x4002104C
 	.equ GPIOB_MODER, 0x48000400
 	.equ GPIOC_MODER, 0x48000800
+	.equ BSRR_OFFSET, 0x18
+  	.equ BRR_OFFSET, 0x28
+    	.equ cs, 0x10  //gpiob pin4 0b10000
+    	.equ din, 0x8  //pb pin3 0b1000
+    	.equ clk, 0x20 //pb pin5 0b100000
+    	.equ DISPLAY_TEST, 0x0F
+    	.equ SCAN_LIMIT, 0x0B
+    	.equ DECODE_MODE, 0x09
+    	.equ INTENSITY, 0x0A
+    	.equ SHUTDOWN, 0x0C
+    	.equ DIGIT0, 0x01
 	.equ OSPEEDR, 0x48000810
 	.equ DELAY, 140000
 	.global delay
 	.global GPIOINIT
-
+	.global display
+	.global MAX7219INIT
 .func delay
 delay:
 	push {r4-r11,lr}
 	ldr r4,=DELAY
 	delay_loop:
-		bl read_button
+		//bl read_button
 		subs r4,#1
 		bne  delay_loop
 	pop {r4-r11,lr}
@@ -48,3 +60,102 @@ GPIOINIT:
 	pop {r4-r11,lr}
 	bx lr
 .endfunc
+
+.func display
+display:
+	push {r4-r11,lr}
+	movs r6, r0
+	movs r2, #10
+	movs r11, #1 //counter
+	next_bit:
+		movs r0, r11
+		sdiv r3, r6, r2
+		mul r5, r3, r2
+		subs r1, r6, r5
+		cmp r11, #3
+		it eq
+		addeq r1, #0xF0
+		BL MAX7219Send
+		add r11, #1
+		movs r6, r3
+		cmp r6, #0
+		bgt next_bit
+		//ble return
+	pop {r4-r11,lr}
+	bx lr
+.endfunc
+.func MAX7219INIT
+MAX7219INIT:
+	push {r4-r11,lr}
+	ldr r0, =#DECODE_MODE
+	ldr r1, =#0xFF
+	BL MAX7219Send
+	ldr r0, =#DISPLAY_TEST
+	ldr r1, =#0x00
+	BL MAX7219Send
+	ldr r0, =#SCAN_LIMIT
+	ldr r1, =0x07
+	BL MAX7219Send
+	ldr r0, =#INTENSITY
+	ldr r1, =#0x0f
+	BL MAX7219Send
+	ldr r0, =#SHUTDOWN
+	ldr r1, =#0x01
+	BL MAX7219Send
+	//ldr r0, =DIGIT0
+	//movs r1, #1
+	movs r0, #3
+	movs r1, #0x80
+	BL MAX7219Send
+	movs r0, #1
+	movs r1, #0x0F
+	BL MAX7219Send
+	movs r0, #2
+	movs r1, #0x0F
+	BL MAX7219Send
+	movs r0, #4
+	movs r1, #0x0F
+	BL MAX7219Send
+	movs r0, #5
+	movs r1, #0x0F
+	BL MAX7219Send
+	movs r0, #6
+	movs r1, #0x0F
+	BL MAX7219Send
+	movs r0, #7
+	movs r1, #0x0F
+	BL MAX7219Send
+	movs r0, #8
+	movs r1, #0x0F
+	BL MAX7219Send
+	pop {r4-r11,pc}
+.endfunc
+MAX7219Send:
+	push {r0, r1, r2, r3, r4-r11, lr}
+    lsl r0, r0, #8
+    add r0, r0, r1
+    ldr r1, =GPIOB_MODER
+    ldr r2, =#cs
+    ldr r3, =#din
+    ldr r4, =#clk
+    ldr r5, =#BSRR_OFFSET
+    ldr r6 , =#BRR_OFFSET
+    movs r7, #16
+    send_loop:
+		movs r8, #1
+		sub r9, r7, #1
+		lsl r8, r8, r9
+		str r4, [r1, r6] // reset
+		tst r0, r8
+		beq not_set
+		str r3, [r1, r5] // set din
+		b if_done
+	not_set:
+		str r3, [r1, r6] // reset din
+	if_done:
+		str r4, [r1, r5] // set .... clk trigger
+		subs r7, r7, #1
+		bgt send_loop
+		str r2, [r1, r6] // reset
+		str r2, [r1, r5] // set .... cs trigger
+    		pop {r0, r1, r2, r3, r4-r11, pc}
--- main.c
+++ main.c
@@ -1,39 +1,15 @@
 #include <stdint.h>
 #include "stm32l476xx.h"
+#define TIME_SEC 6.73
 extern void delay();
 extern void GPIOINIT();
+extern void display(int val);
+extern void MAX7219INIT();
 int pattern = 0;
 int freqs[5] = {1,6,10,16,40};
-void system_clock_config(int press_num){
-	if(press_num == 0 || press_num == 3){
-		// 1M Hz
-		RCC->CR |= RCC_CR_HSION;
-		while((RCC->CR & RCC_CR_HSIRDY) == 0);
-		RCC->CFGR &= 0xfffff000;
-		if(press_num == 0)
-			RCC->CFGR |= 11<<4;
-		RCC->CFGR |= 5;
-	}
-	else if(press_num == 1){
-		// 6M Hz
-		RCC->CFGR &= 0xfffff000;
-		RCC->CR &= 0xFEFFFFFF;
-		while((RCC->CR & RCC_CR_PLLRDY) > 0);
-		RCC->PLLCFGR &= 0xf9ff808f;
-		RCC->PLLCFGR |= 2; //set clock source
-		RCC->PLLCFGR |= RCC_PLLCFGR_PLLN_3;
-		RCC->PLLCFGR |= RCC_PLLCFGR_PLLN_4;
-		RCC->PLLCFGR |= RCC_PLLCFGR_PLLM_0;
-		RCC->PLLCFGR |= RCC_PLLCFGR_PLLM_1;
-		RCC->PLLCFGR |= RCC_PLLCFGR_PLLM_2;
-		RCC->PLLCFGR |= RCC_PLLCFGR_PLLR_0;
-		RCC->PLLCFGR |= RCC_PLLCFGR_PLLR_1;
-		RCC->CR |= RCC_CR_PLLON;
-		RCC->CR |= RCC_CR_HSION;
-		RCC->PLLCFGR |= RCC_PLLCFGR_PLLREN;
-		RCC->CFGR |= 15;
-	}
-	else if(press_num == 2){
+uint32_t TIM_ARR_VAL = 9999;
+uint32_t TIM_PSC_VAL = 999;
+void system_clock_config(){
 		// 10M Hz
 		RCC->CFGR &= 0xfffff000;
 		RCC->CR &= 0xFEFFFFFF;
@@ -51,60 +27,37 @@ void system_clock_config(int press_num){
 		RCC->CR |= RCC_CR_HSION;
 		RCC->PLLCFGR |= RCC_PLLCFGR_PLLREN;
 		RCC->CFGR |= 15;
-	}
-	else if(press_num == 4){
-		// 40M Hz
-		RCC->CFGR &= 0xfffff000;
-		RCC->CR &= 0xFEFFFFFF;
-		while((RCC->CR & RCC_CR_PLLRDY) > 0);
-		RCC->PLLCFGR &= 0xf9ff808f;
-		RCC->PLLCFGR |= 2; //set clock source
-		RCC->PLLCFGR |= RCC_PLLCFGR_PLLN_2;
-		RCC->PLLCFGR |= RCC_PLLCFGR_PLLN_3;
-		RCC->PLLCFGR |= RCC_PLLCFGR_PLLN_4;
-		RCC->PLLCFGR |= RCC_PLLCFGR_PLLN_5;
-		RCC->PLLCFGR |= RCC_PLLCFGR_PLLM_1;
-		RCC->PLLCFGR |= RCC_PLLCFGR_PLLR_0;
-		RCC->PLLCFGR |= RCC_PLLCFGR_PLLR_1;
-		RCC->CR |= RCC_CR_PLLON;
-		RCC->CR |= RCC_CR_HSION;
-		RCC->PLLCFGR |= RCC_PLLCFGR_PLLREN;
-		RCC->CFGR |= 15;
-	}
-
 }
-void debounce(int freq){
-	int k =0 ;
-	for(int i=550*freq;i>=0;i--){
-		k++;
-	}
+void initailize_timer(){
+	RCC->APB1ENR1 |= RCC_APB1ENR1_TIM2EN;
+	TIM2->CR1 &= (!(1<<4));
+	TIM2->ARR = (uint32_t)TIM_ARR_VAL;
+	TIM2->PSC = (uint32_t)TIM_PSC_VAL;
+	TIM2->EGR = TIM_EGR_UG;
 }
-void read_button(){
-	int button = (GPIOC->IDR & (1<<13)) >> 13;
-	if(!button){
-
-		debounce(freqs[pattern]);
-		button = (GPIOC->IDR & (1<<13)) >> 13;
-		if (!button){
-			return;
-		}
-		debounce(freqs[pattern]);
-	}
-	else{
-		return;
-	}
-	pattern++;
-	pattern = pattern % 5;
-	system_clock_config(pattern);
-}
-
-int main(){
-	system_clock_config(0);
-	GPIOINIT();
+void timer_start(){
+	TIM2->CR1 |= 1;
+	int pre = 0;
+	int sec = 0;
 	while(1){
-		GPIOB->ODR |= 8;
-		delay();
-		GPIOB->ODR &= 0;
-		delay();
+		int timer_value = TIM2->CNT;
+		if (pre > timer_value){
+			sec ++;
+		}
+		int limit = TIME_SEC * 10000;
+		if (limit < (sec*10000 + timer_value)){
+			display(limit / 100);
+			break;
+		}
+		pre = timer_value;
+		timer_value /= 100;
+		display(sec*100 + timer_value);
 	}
+}
+int main(){
+	system_clock_config();
+	GPIOINIT();
+	MAX7219INIT();
+	initailize_timer();
+	timer_start();
 }
