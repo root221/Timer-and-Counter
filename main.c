fdb548aac403e1004138c3b419d4ef8039c9c4c4
skye <skye53653@gmail.com>
2016-12-13 19:22:23 +0800

    8-2 can work but rising

--- main.c
+++ main.c
@@ -8,6 +8,7 @@ void systick_init(){
 	SysTick->CTRL |= SysTick_CTRL_TICKINT_Msk;
 	SysTick->CTRL |= SysTick_CTRL_CLKSOURCE_Msk;
 	SysTick->LOAD = (uint32_t) 1000000;
+
 }
 void system_clock_config(){
 		// 10M Hz
@@ -40,16 +41,19 @@ void GPIO_init(){
 	GPIOC->OSPEEDR = GPIOC->OSPEEDR | 0x5500;
 	GPIOC->PUPDR = GPIOC->PUPDR | 0xAA;
 }
-int col = 0;
+volatile int col = 0;
 void SysTick_Handler(){
+
+	NVIC->ICER[0] = (uint32_t) 0x3C0;
 	col++;
 	col %= 4;
-	GPIOC->ODR = 0;
-	GPIOC->MODER = 0;
-	GPIOC->MODER = 1 << ((col+4)*2);
-	GPIOC->PUPDR = GPIOC->PUPDR | 0xAA;
+	//GPIOC->ODR = 0;
 	GPIOC->ODR = 1 << (col + 4);
-	EXTI_Setup();
+	NVIC->ISER[0] = (uint32_t) 0x3C0;
+	//EXTI->PR1 |= 0xf;
+	//EXTI_Setup();
+
+
 }
 
 void debounce(){
@@ -63,79 +67,46 @@ void EXTI_Setup(){
 	SYSCFG->EXTICR[0] = 0;
 	SYSCFG->EXTICR[0] |= (uint32_t)0x2222; // 0 010 PC
 	EXTI->IMR1 = (uint32_t) 0xF;
-	EXTI->PR1 = 0xf;
-	EXTI->FTSR1 = (uint32_t) 0xF;
-	NVIC->IP[6] = (uint8_t)5;
-	//NVIC_SetPriority(6, 5);
+	//EXTI->PR1 = 0xf;
+	EXTI->RTSR1 = (uint32_t) 0xF;
+	NVIC_SetPriority(6, (uint32_t)5);
 	NVIC_SetPriority(7, 5);
 	NVIC_SetPriority(8, 5);
 	NVIC_SetPriority(9, 5);
-	NVIC->ISER[0] = (uint32_t) 0x3C0;
-}
-int key = 0;
-int Array[15]={1,2,3,10,4,5,6,11,7,8,9,12,15,0,14,13};
-void EXTI0_IRQHandler(void){
-	debounce();
-	int value = GPIOC->IDR ;
-	value &= 0xf;
-	if (value == 0){
 
-	}
-	else if (value == 1){
+}
+volatile int key = 0;
+volatile int Array[15]={1,2,3,10,4,5,6,11,7,8,9,12,15,0,14,13};
+void EXTI0_IRQHandler(void){
+	int value = EXTI->PR1;
+	if(value==1)
 		value = 0;
-	}
-	else if(value == 2){
+	else if(value==2)
 		value = 1;
-	}
-	else if(value == 4){
+	else if(value==4)
 		value = 2;
-	}
-	else if(value == 8){
+	else if(value==8)
 		value = 3;
-	}
+
+
 	key = Array[4*value+col];
-	EXTI->IMR1 = 0;
+	display(key);
+	debounce();
+	//EXTI->IMR1 = 0;
 	EXTI->PR1 |= 0xf;
-	NVIC->ICER[0] = 0x3c0;
-	NVIC->ICPR[0] = 0x3c0;
+	//NVIC->ICER[0] = 0x3c0;
+	//NVIC->ICPR[0] = 0x3c0;
 }
-/*
-void EXTI1_IRQHandler(void){
-	debounce();
-	int a = GPIOC->IDR;
-	key = Array[4+col];
-	EXTI->PR1 = 0xf;
-	EXTI->IMR1 = 0;
-	NVIC->ICER[0] = 0x3c0;
-	NVIC->ICPR[0] = 0x3c0;
-}
-void EXTI2_IRQHandler(void){
-	debounce();
-	int a = GPIOC->IDR;
-	key = Array[8+col];
-	EXTI->PR1 = (uint32_t)0xf;
-	EXTI->IMR1 = 0;
-	NVIC->ICER[0] = 0x3c0;
-	NVIC->ICPR[0] = 0x3c0;
-}
-void EXTI3_IRQHandler(void){
-	debounce();
-	int a = GPIOC->IDR;
-	key = Array[12+col];
-	EXTI->PR1 = 0xf;
-	EXTI->IMR1 = 0;
-	NVIC->ICER[0] = 0x3c0;
-	NVIC->ICPR[0] = 0x3c0;
-}
-*/
+
 int main(){
 	system_clock_config();
 	GPIO_init();
 	systick_init();
 	MAX7219INIT();
-//	EXTI_Setup();
+	EXTI_Setup();
+
 	while(1){
-		display(key);
+
 
 	}
 }
